afcab6955931d0efe321d49ea0972151c1035d0b
Thomas Tuts <thomas@thomastuts.com>
2015-11-14 13:58:10 +0100

    Use ES7 async/await to seed all data sequentially

--- db/seeds/index.js
+++ db/seeds/index.js
@@ -2,13 +2,25 @@ var connectionInfo = require('../../knexfile').development;
 var knex = require('knex')(connectionInfo);
 var users = require('./users');
 var posts = require('./posts');
+var tags = require('./tags');
+var postsTags = require('./posts-tags');
 
 console.log('Seeding database...');
 
-// TODO: get rid of this promise chaining, it's just awful
-users.seed(knex).then(function () {
-  posts.seed(knex).then(function () {
-    console.log('Seeding done!');
+async function seedDatabase() {
+  try {
+    await users.seed(knex);
+    await posts.seed(knex);
+    await tags.seed(knex);
+    await postsTags.seed(knex);
+    console.log('Done seeding!');
     process.exit(0);
-  });
-});
+  }
+  catch (e) {
+    console.log('Error during seeding:');
+    console.log(e);
+    process.exit(1);
+  }
+}
+
+seedDatabase();
